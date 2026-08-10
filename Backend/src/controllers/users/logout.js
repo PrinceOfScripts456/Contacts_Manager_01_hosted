e43d4bfbb2adfea40bfa539ebaf55ddca953fbc1
Prince Vegeta <PrinceofBugs001@gmail.com>
2026-08-10 15:33:53 +0500

    minor fix

--- Backend/src/controllers/users/logout.js
+++ Backend/src/controllers/users/logout.js
@@ -2,7 +2,7 @@ const logout = (req, res) => {
     res.clearCookie("token", {
         httpOnly: true,
         secure: true,
-        sameSite: "lax",
+        sameSite: "none",
     });
 
     return res.status(200).json({ message: "Logged out successfully" });
