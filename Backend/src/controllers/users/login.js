c8e0f1f662a2dc1fd581738a0715fd5ce397bbde
Prince Vegeta <PrinceofBugs001@gmail.com>
2026-08-10 15:33:34 +0500

    minor fix

--- Backend/src/controllers/users/login.js
+++ Backend/src/controllers/users/login.js
@@ -20,7 +20,7 @@ const login = async (req, res, next) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,
-            sameSite: "lax",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
 
